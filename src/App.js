e822d5f195fefe3e0d116806b2f9ae3838685ddb
Shaun Yarbrough <ycorredius@gmail.com>
2020-04-08 23:32:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent';
 import GraceHopperQuoteComponent from './GraceHopperQuoteComponent';
+import MouseComponent from './MouseComponent';
 
 class App extends Component {
 	render() {
